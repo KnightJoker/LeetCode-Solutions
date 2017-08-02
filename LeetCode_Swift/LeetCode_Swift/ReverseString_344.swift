f4b36374d3f11a9e78735eeac423077ae090ec88
KnightJoker <cbearxl@gmail.com>
2017-08-02 14:30:38 +0800

    【update】344_solution in swift

---
+++ LeetCode_Swift/LeetCode_Swift/ReverseString_344.swift
@@ -0,0 +1,15 @@
+//
+//  ReverseString_344.swift
+//  LeetCode_Swift
+//
+//  Created by Huni on 02/08/2017.
+//  Copyright © 2017 KnighhtJoker. All rights reserved.
+//
+
+import Foundation
+
+public class ReverseString_344 {
+    func reverseString(_ s: String) -> String {
+        return String(s.characters.reversed())
+    }
+}
---
+++ LeetCode_Swift/LeetCode_Swift/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  LeetCode_Swift
+//
+//  Created by Huni on 02/08/2017.
+//  Copyright © 2017 KnighhtJoker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let a = ReverseString_344()
+
+print(a.reverseString("abcdefg"))
+
+
