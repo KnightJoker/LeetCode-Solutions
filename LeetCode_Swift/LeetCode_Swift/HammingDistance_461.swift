512bfd9677bd44343402e6f4c37f2017e8c72261
KnightJoker <cbearxl@gmail.com>
2017-08-24 10:57:33 +0800

    【update】solution_461 in swift

---
+++ LeetCode_Swift/LeetCode_Swift/HammingDistance_461.swift
@@ -0,0 +1,35 @@
+//
+//  HammingDistance_461.swift
+//  LeetCode_Swift
+//
+//  Created by Huni on 24/08/2017.
+//  Copyright © 2017 KnighhtJoker. All rights reserved.
+//
+
+import Foundation
+
+public class HammingDistance_461 {
+
+    func hammingDistance(_ x: Int, _ y: Int) -> Int {
+        
+        var res = 0
+        var exc = x ^ y
+        while exc != 0 {
+            res += 1
+            exc &= (exc - 1)
+        }
+        return res
+    }
+}
+
+/*
+ 
+ NSInteger res = 0;
+ NSInteger exc = x ^ y;
+ while (exc) {
+ ++res;
+ exc &= (exc - 1);
+ }
+ return res;
+ 
+ */
--- LeetCode_Swift/LeetCode_Swift/main.swift
+++ LeetCode_Swift/LeetCode_Swift/main.swift
@@ -41,7 +41,7 @@ print("Hello, World!")
  
  */
 
-let a = ClimbingStairs_70()
+let a = HammingDistance_461()
 
 let tree = TreeNode.init(1)
 let tree1 = TreeNode.init(2)
@@ -59,5 +59,5 @@ tree1.right = tree4
  "6529364523802684779"
  */
 
-print(a.climbStairs(44))
+print(a.hammingDistance(1, 4))
 
