59600e75f9f986ceeae58d708dc97f87796b0f04
KnightJoker <cbearxl@gmail.com>
2017-09-25 10:35:06 +0800

    【update】solution_541 in swift

---
+++ LeetCode_Swift/LeetCode_Swift/ReverseStringII_541.swift
@@ -0,0 +1,32 @@
+//
+//  ReverseStringII_541.swift
+//  LeetCode_Swift
+//
+//  Created by Huni on 25/09/2017.
+//  Copyright © 2017 KnighhtJoker. All rights reserved.
+//
+
+import Foundation
+
+class ReverseStringII_541 {
+    func reverseStr(_ s: String, _ k: Int) -> String {
+        
+        var s = Array(s.characters)
+        
+        for i in stride(from: 0, to: s.count, by: 2 * k) {
+            
+            var i = i
+            var j = min(i + k - 1, s.count - 1)
+            
+            while i < j {
+                let temp = s[i]
+                s[i] = s[j]
+                s[j] = temp
+                
+                i += 1
+                j -= 1
+            }
+        }
+        return String(s)
+    }
+}
