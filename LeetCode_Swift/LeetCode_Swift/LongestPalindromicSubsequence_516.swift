f3fbd5c90357f4bf8142f0600f4d686f9bac4df7
KnightJoker <cbearxl@gmail.com>
2018-04-09 23:43:57 +0800

    【BugFix】

--- LeetCode_Swift/LeetCode_Swift/LongestPalindromicSubsequence_516.swift
+++ LeetCode_Swift/LeetCode_Swift/LongestPalindromicSubsequence_516.swift
@@ -12,12 +12,12 @@ class LongestPalindromicSubsequence_516 {
     func longestPalindromeSubseq(_ s: String) -> Int {
         
         var dp = Array(repeating: Array(repeating: 0, count: s.count), count: s.count)
-        let sArray = Array(s.characters)
+        let sArray = Array(s)
         
-        var i = s.characters.count - 1
+        var i = s.count - 1
         while i >= 0 {
             dp[i][i] = 1
-            for j in (i+1)..<s.characters.count {
+            for j in (i+1)..<s.count {
                 if sArray[i] == sArray[j] {
                     dp[i][j] = dp[i + 1][j - 1] + 2
                 } else {
@@ -26,7 +26,7 @@ class LongestPalindromicSubsequence_516 {
             }
             i -= 1
         }
-        return dp[0][s.characters.count - 1]
+        return dp[0][s.count - 1]
     }
 
 }
